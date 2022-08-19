d5da2e17d7beb3ca48d99a5c136bf534217bf840
Rachel Berryman <rachelkberryman@gmail.com>
2022-08-19 19:38:43 +0200

    making prediction application, starting instruction notebook

---
+++ prediction_streamlit_app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
+
+def load_pickles(model_pickle_path, label_encoder_pickle_path):
+    model_pickle_opener = open(model_pickle_path, "rb")
+    model = pickle.load(model_pickle_opener)
+
+    label_encoder_pickle_opener = open(label_encoder_pickle_path, "rb")
+    label_encoder_dict = pickle.load(label_encoder_pickle_opener)
+
+    return model, label_encoder_dict
+
+
+def pre_process_data(df, label_encoder_dict):
+    df.drop("customerID", axis=1, inplace=True)
+    for col in df.columns:
+        if col in list(label_encoder_dict.keys()):
+            column_le = label_encoder_dict[col]
+            df.loc[:, col] = column_le.transform(df.loc[:, col])
+        else:
+            continue
+    # TODO: add assert statement that all cols are numeric
+    return df
+
+
+def make_predictions(processed_df, model):
+    prediction = model.predict(processed_df)
+    return prediction
+
+
+def generate_predictions(test_df):
+    model_pickle_path = "./churn_model/churn_prediction_model.pkl"
+    label_encoder_pickle_path = "./churn_model/churn_prediction_label_encoders.pkl"
+
+    model, label_encoder_dict = load_pickles(model_pickle_path,
+                                             label_encoder_pickle_path)
+
+    processed_df = pre_process_data(test_df, label_encoder_dict)
+    prediction = make_predictions(processed_df, model)
+    return prediction
+
+
+if __name__ == '__main__':
+    # make the application
+    st.title("Customer Churn Prediction")
+    all_customers_training_data = pd.read_csv("./data/holdout_data.csv")
+    all_customers_data = all_customers_training_data.drop(columns="Churn")
+    chosen_customer = st.selectbox("Select the customer you are speaking to:", all_customers_training_data.loc[:, "customerID"])
+    chosen_customer_data = all_customers_data.loc[all_customers_data.loc[:, 'customerID']==chosen_customer]
+    # generate the prediction for the customer
+    pred = generate_predictions(chosen_customer_data)
+    st.text(pred)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+import matplotlib.pyplot as plt
 
-df
+# importing customer churn data
+data = pd.read_csv("./data/WA_Fn-UseC_-Telco-Customer-Churn.csv", index_col=0)
+st.write("Churn Data")
+st.write(data)
+
+st.write("How many customers in the dataset churned?")
+target_bins = data.loc[:, 'Churn'].value_counts()
+st.bar_chart(target_bins)
