bcb8171c80897c309dc4de0a0f68bd2a240d232c
emre-oym <137804567+emre-oym@users.noreply.github.com>
2023-07-27 13:44:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ data = pd.read_csv("./data/training_data.csv", index_col=0)
 st.write("Churn Data")
 st.write(data)
 
-st.write("How many customers in the dataset churned?")
+st.write("How many customers in the dataset churned eyyy?")
 target_bins = data.loc[:, 'Churn'].value_counts()
 st.bar_chart(target_bins)
